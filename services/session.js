b629c22aae3caf08e4d8ce3fa2062a7a8e9873c0
knockknockyoo <iheaven0129@gmail.com>
2023-05-22 11:24:50 -0400

    Mongo DB Connection

--- services/session.js
+++ services/session.js
@@ -2,6 +2,7 @@ const session = require('express-session');
 const {randomBytes} = require("crypto");
 const MongoStore = require('connect-mongo');
 const config = require('../config');
+const {ConnectionString} = require("../database-drivers/database-connector");
 function createSession({ sessionSecret, session_timeout } = {}) {
     sessionSecret = sessionSecret || randomBytes(16).toString("hex");
     return session({
@@ -10,7 +11,7 @@ function createSession({ sessionSecret, session_timeout } = {}) {
         saveUninitialized: false,
         resave: true,
         store: MongoStore.create({
-            mongoUrl: `mongodb://${config.mongo_db_user}:${config.mongo_db_password}@${config.mongo_db_host}:${config.mongo_db_port}`,
+            mongoUrl: ConnectionString,
             touchAfter: session_timeout // time period in seconds
         })
     });
