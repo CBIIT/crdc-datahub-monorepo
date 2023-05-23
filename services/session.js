f62f01a384b08ca9fb00125f0f0dd4763d94ba26
knockknockyoo <iheaven0129@gmail.com>
2023-05-23 09:59:24 -0400

    Moved Mongo DB Storage.

--- services/session.js
+++ services/session.js
@@ -1,7 +1,7 @@
 const session = require('express-session');
 const {randomBytes} = require("crypto");
-const MongoStore = require('connect-mongo');
 const config = require('../config');
+const {DatabaseConnector} = require("../database-drivers/database-connector");
 function createSession({ sessionSecret, session_timeout } = {}) {
     sessionSecret = sessionSecret || randomBytes(16).toString("hex");
     return session({
@@ -9,10 +9,7 @@ function createSession({ sessionSecret, session_timeout } = {}) {
         // rolling: true,
         saveUninitialized: false,
         resave: true,
-        store: MongoStore.create({
-            mongoUrl: config.mongo_db_connection_string,
-            touchAfter: session_timeout // time period in seconds
-        })
+        store: DatabaseConnector.createMongoStore(config.mongo_db_connection_string, session_timeout)
     });
 }
 
