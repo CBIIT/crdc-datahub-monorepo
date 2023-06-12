3bb06a42869772edbfea9c079125154ce67ebe79
Austin Mueller <austinsmueller@gmail.com>
2023-06-12 15:13:13 -0400

    Updated sub-module
    - updated to the latest sub-module
    - updated dependencies to resolve vulnerabilities

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -1,20 +1,22 @@
 const {MongoQueries} = require("../crdc-datahub-database-drivers/mongo-queries");
+const config = require("../config");
+const {DATABASE_NAME} = require("../crdc-datahub-database-drivers/database-constants");
 
 describe('Mongo DB Test', () => {
     test('/find document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         const result = await mongoQueries.find("users", {name: 'John'})
         console.log(result);
     });
 
     test('/insert document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         const result = await mongoQueries.insertOne("users", {name: 'mongo users'})
         console.log("")
     });
 
     test('/update one document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         await mongoQueries.updateOne("users", { "name": "updated name"},
             {
                 $set: { "name": "John", city: "Seoul" }
@@ -22,7 +24,7 @@ describe('Mongo DB Test', () => {
     });
 
     test('/update many document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         await mongoQueries.updateMany("users", { name: "John"},
             {
                 $set: { "name": "John test", city: "New York" }
@@ -30,12 +32,12 @@ describe('Mongo DB Test', () => {
     });
 
     test('/delete one document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         await mongoQueries.deleteOne("users", { name: "John" });
     });
 
     test('/delete many document',  async () => {
-        const mongoQueries = new MongoQueries();
+        const mongoQueries = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
         await mongoQueries.deleteMany("users", { age: {$lt : 20}  })
     });
 
