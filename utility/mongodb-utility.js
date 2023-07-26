46160bca839e32787a7b0c30b00a60cbcdc4489b
knockknockyoo <iheaven0129@gmail.com>
2023-07-26 16:53:26 -0400

    simplified

--- utility/mongodb-utility.js
+++ utility/mongodb-utility.js
@@ -1,8 +1,7 @@
 const {DIRECTION, SORT} = require("../constants/monogodb-constants");
 module.exports = {
     getSortDirection(direction) {
-        if (!direction) return DIRECTION.DESC;
-        if (direction.toLowerCase() === SORT.ASC) return DIRECTION.ASC;
+        if (direction && direction.toLowerCase() === SORT.ASC) return DIRECTION.ASC;
         return DIRECTION.DESC;
     }
 }
