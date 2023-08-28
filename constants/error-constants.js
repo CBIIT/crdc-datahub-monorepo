b93ea12af988c6212758278949dafedaf6375f93
Alec M <alec.mattu@nih.gov>
2023-08-28 14:27:13 -0400

    CRDCDH-298 Enable getUser API

---
+++ constants/error-constants.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    ERROR: {
+        NOT_LOGGED_IN: "A user must be logged in to call this API",
+        INVALID_USERID: "A userID argument is required to call this API",
+        INVALID_ROLE: "You do not have the correct role to perform this operation",
+    },
+});
