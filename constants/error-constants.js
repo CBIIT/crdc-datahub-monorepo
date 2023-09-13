66698620ec1900d91b80b200d5a17ef4af1ab5dd
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2023-09-13 16:46:11 -0400

    update error messages

--- constants/error-constants.js
+++ constants/error-constants.js
@@ -2,7 +2,8 @@ module.exports = Object.freeze({
     ERROR: {
         // TODO CRDC backend already has these error constants, let's merge it.
         NOT_LOGGED_IN: "A user must be logged in to call this API",
-        INVALID_USER_STATUS: "A user with an invalid status is prohibited from logging in. Please, verify that your account is disabled.",
+        INACTIVE_USER: "Login Failed: This user account has been marked as inactive and must be reactivated before it can be used",
+        INVALID_USER_STATUS: "This user account has does not have the correct status to perform this operation",
         INVALID_USERID: "A userID argument is required to call this API",
         INVALID_ROLE: "You do not have the correct role to perform this operation",
         NO_ORG_ASSIGNED: "You do not have an organization assigned to your account",
