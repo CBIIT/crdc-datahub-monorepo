4dd4cb5e9f7b3f35d7327b0b84d09464a055a1da
Wesley Lau <wesley.lau@nih.org>
2023-09-18 09:42:48 -0400

    Updated session-middleware to accommodate the session-ttl changes

--- session-middleware.js
+++ session-middleware.js
@@ -3,19 +3,22 @@ const {DATABASE_NAME, SESSION_COLLECTION} = require("./database-constants");
 const MongoStore = require("connect-mongo");
 
 function createSession(sessionSecret, sessionTimeout, connectionString) {
-    return session({
-        secret: sessionSecret,
-        // rolling: true,
-        saveUninitialized: false,
-        resave: true,
-        store: MongoStore.create({
-            mongoUrl: connectionString,
-            collectionName: SESSION_COLLECTION,
-            dbName: DATABASE_NAME,
-            touchAfter: sessionTimeout // time period in seconds
-        }),
-        cookie: {maxAge: sessionTimeout}
-    });
+    return function (req, res, next) {
+        const disableResave = !(req.url === '/api/authn/session-ttl');
+        session({
+            secret: sessionSecret,
+            // rolling: true,
+            saveUninitialized: false,
+            resave: disableResave,
+            store: MongoStore.create({
+                mongoUrl: connectionString,
+                collectionName: SESSION_COLLECTION,
+                dbName: DATABASE_NAME,
+                touchAfter: sessionTimeout // time period in seconds
+            }),
+            cookie: { maxAge: sessionTimeout}
+        })(req, res, next);
+    };
 }
 
 module.exports = createSession;
