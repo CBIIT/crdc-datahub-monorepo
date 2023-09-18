cc89224b0e213036773b2aabd94adf66230dc905
Michael Tang <mtangmt@gmail.com>
2023-09-18 15:58:35 -0400

    adding data submissions collection

--- database-constants.js
+++ database-constants.js
@@ -3,6 +3,7 @@ module.exports = Object.freeze({
     DATABASE_NAME: 'crdc-datahub',
     SESSION_COLLECTION: 'sessions',
     APPLICATION_COLLECTION: 'applications',
+    DATA_SUBMISSIONS_COLLECTION: 'data_submissions',
     USER_COLLECTION: 'users',
     ORGANIZATION_COLLECTION: 'organization',
     LOG_COLLECTION: 'logs'
