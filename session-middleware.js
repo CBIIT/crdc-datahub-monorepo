db37106a7329ceffd3a124913b72a35bebb4f69f
Wesley Lau <wesley.lau@nih.org>
2023-09-20 14:47:28 -0400

    Enabled rolling to refresh session when any api called is made.

--- session-middleware.js
+++ session-middleware.js
@@ -3,22 +3,19 @@ const {DATABASE_NAME, SESSION_COLLECTION} = require("./database-constants");
 const MongoStore = require("connect-mongo");
 
 function createSession(sessionSecret, sessionTimeout, connectionString) {
-    return function (req, res, next) {
-        const disableResave = !(req.url === '/api/authn/session-ttl');
-        session({
-            secret: sessionSecret,
-            // rolling: true,
-            saveUninitialized: false,
-            resave: disableResave,
-            store: MongoStore.create({
-                mongoUrl: connectionString,
-                collectionName: SESSION_COLLECTION,
-                dbName: DATABASE_NAME,
-                touchAfter: sessionTimeout // time period in seconds
-            }),
-            cookie: { maxAge: sessionTimeout}
-        })(req, res, next);
-    };
+    return session({
+        secret: sessionSecret,
+        rolling: true,
+        saveUninitialized: false,
+        resave: true,
+        store: MongoStore.create({
+            mongoUrl: connectionString,
+            collectionName: SESSION_COLLECTION,
+            dbName: DATABASE_NAME,
+            touchAfter: sessionTimeout // time period in seconds
+        }),
+        cookie: {maxAge: sessionTimeout}
+    });
 }
 
 module.exports = createSession;
