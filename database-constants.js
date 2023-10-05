0447e06fc5cbd15a241467ab68a0d58233f61df2
Michael Tang <mtangmt@gmail.com>
2023-10-05 14:50:46 -0400

    changing dataSubmissions to submissions

--- database-constants.js
+++ database-constants.js
@@ -1,9 +1,9 @@
 module.exports = Object.freeze({
     // MongoDB Collections
-    DATABASE_NAME: 'crdc-datahub2',
+    DATABASE_NAME: 'crdc-datahub',
     SESSION_COLLECTION: 'sessions',
     APPLICATION_COLLECTION: 'applications',
-    DATA_SUBMISSIONS_COLLECTION: 'data_submissions',
+    SUBMISSIONS_COLLECTION: 'submissions',
     APPROVED_STUDIES_COLLECTION: 'approvedStudies',
     USER_COLLECTION: 'users',
     ORGANIZATION_COLLECTION: 'organization',
