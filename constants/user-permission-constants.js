1d90334a4d8f6c37711c55b01430222c3c67e2a0
knockknockyoo <iheaven0129@gmail.com>
2024-12-09 14:05:19 -0500

    update it to String Array

---
+++ constants/user-permission-constants.js
@@ -0,0 +1,45 @@
+module.exports = Object.freeze({
+    EMAIL_NOTIFICATIONS: {
+        SUBMISSION: {
+            SUBMIT: "Submit",
+            APPROVE: "Approve",
+            CANCEL: "Cancel",
+            REJECT: "Reject",
+            RELEASE: "Release",
+            COMPLETE: "Complete",
+            DELETE: "Delete",
+            WITHDRAW: "Withdraw",
+            REMIND_EXPIRE: "Remind Expire"
+        },
+        APPLICATION: {
+            REQUEST_SUBMIT: "Request Submit",
+            REQUEST_APPROVE: "Request Approve",
+            REQUEST_REJECT: "Request Reject",
+            REQUEST_ADDITIONAL_INFO: "Request Additional Info"
+        }
+    },
+    // TODO By Mr. Peter
+    APPLICATION_ACTION: {
+        VIEW: "View",
+        CREATE: "Create",
+        SUBMIT: "Submit",
+        REVIEW: "Review"
+    },
+    SUBMISSION_ACTION: {
+        CREATE: "Create",
+        UPLOAD: "Upload",
+        SUBMIT: "Submit",
+        WITHDRAW: "Withdraw",
+        CANCEL: 'Cancel',
+        VALIDATE: 'Validate',
+        CROSS_VALIDATE: 'CrossValidate',
+        RELEASE: 'Release',
+        REJECT: 'Reject',
+        ADMIN_SUBMIT: 'AdminSubmit',
+        COMPLETE_RELEASE: 'CompleteRelease',
+        REJECT_RELEASE: 'RejectRelease'
+    },
+    USER_ACTION: {
+        REQUEST_ACCESS: "RequestAccess"
+    }
+});
