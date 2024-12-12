7b5be3a0cb1072de49eda024993f6dd14cbac2c8
knockknockyoo <iheaven0129@gmail.com>
2024-12-12 14:54:14 -0500

    updated constant value

--- constants/user-permission-constants.js
+++ constants/user-permission-constants.js
@@ -1,41 +1,42 @@
 module.exports = Object.freeze({
     EMAIL_NOTIFICATIONS: {
         SUBMISSION: {
-            SUBMIT: "Submit",
-            CANCEL: "Cancel",
-            RELEASE: "Release",
-            COMPLETE: "Complete",
-            DELETE: "Delete",
-            WITHDRAW: "Withdraw",
-            REMIND_EXPIRE: "Remind Expire"
+            SUBMIT: "Email Submit",
+            CANCEL: "Email Cancel",
+            RELEASE: "Email Release",
+            COMPLETE: "Email Complete",
+            DELETE: "Email Delete",
+            WITHDRAW: "Email Withdraw",
+            REMIND_EXPIRE: "Email Remind Expire"
         },
         APPLICATION: {
-            REQUEST_SUBMIT: "Request Submit",
-            REQUEST_READY_REVIEW: "Request Ready Review",
-            REQUEST_REVIEW: "Request Review"
+            REQUEST_SUBMIT: "RequestEmail Submit",
+            REQUEST_READY_REVIEW: "RequestEmail Ready Review",
+            REQUEST_REVIEW: "RequestEmail Review"
         }
     },
     // TODO By Mr. Peter
     APPLICATION_ACTION: {
-        VIEW: "View",
-        CREATE: "Create",
-        SUBMIT: "Submit",
-        REVIEW: "Review",
+        VIEW: "RequestAction View",
+        CREATE: "RequestAction Create",
+        SUBMIT: "RequestAction Submit",
+        REVIEW: "RequestAction Review",
     },
     SUBMISSION_ACTION: {
-        CREATE: "Create",
-        VALIDATE: 'Validate',
-        REVIEW: 'Review',
-        ADMIN_SUBMIT: 'AdminSubmit',
-        CONFIRM: 'Confirm'
+        VIEW: "SubmissionAction View",
+        CREATE: "SubmissionAction Create",
+        VALIDATE: 'SubmissionAction Validate',
+        REVIEW: 'SubmissionAction Review',
+        ADMIN_SUBMIT: 'SubmissionAction AdminSubmit',
+        CONFIRM: 'SubmissionAction Confirm'
     },
     OTHER_ACTION: {
-        DASHBOARD: "Dashboard",
+        DASHBOARD_VIEW: "Dashboard View",
         MANAGE_PROGRAMS: "Manage Programs",
         MANAGE_STUDIES: "Manage Studies"
     },
     USER_ACTION: {
         MANAGE_USER : "Manage User",
-        REQUEST_ACCESS: "RequestAccess"
+        REQUEST_ACCESS: "User RequestAccess"
     }
 });
