0b20f8cf8622a03dd7e55c2d45bc2d02c4218041
Tracy Truong <tracy18@users.noreply.github.com>
2025-05-02 19:42:03 -0400

    modified the CDK services stacks

--- awscdk/crdcdh/services/essentialvalidation.py
+++ awscdk/crdcdh/services/essentialvalidation.py
@@ -7,12 +7,14 @@ from aws_cdk import aws_secretsmanager as secretsmanager
 from datetime import date
 from aws_cdk import Duration
 from aws_cdk import aws_applicationautoscaling as appscaling
+from aws_cdk import aws_cloudwatch as cloudwatch
+from aws_cdk import aws_cloudwatch_actions as cw_actions
 
-class backendService:
+class essentialvalidationService:
   def createService(self, config):
 
-    ### Backend Service ###############################################################################################################
-    service = "backend"
+    ### EssentialValidation Service ############################################################################################################
+    service = "essentialvalidation"
 
     # Set container configs
     if config.has_option(service, 'entrypoint'):
@@ -26,9 +28,6 @@ class backendService:
             "VERSION":config[service]['image'],
             "FARGATE":"true",
             "SESSION_SECRET":"abcd256asghaaamnkloofghj",
-            "EMAILS_ENABLED":"true",
-            "EMAIL_SMTP_HOST":"email-smtp.us-east-1.amazonaws.com",
-            "EMAIL_SMTP_PORT":"587",
             "NEW_RELIC_APP_NAME":"{}-{}-{}".format(self.namingPrefix, config['main']['tier'], service),
             "NEW_RELIC_DISTRIBUTED_TRACING_ENABLED":"true",
             "NEW_RELIC_HOST":"gov-collector.newrelic.com",
@@ -39,16 +38,6 @@ class backendService:
             "NRIA_CUSTOM_ATTRIBUTES":"{\"nrDeployMethod\":\"downloadPage\"}",
             "NRIA_OVERRIDE_HOST_ROOT":"",
             "JAVA_OPTS": "-javaagent:/usr/local/tomcat/newrelic/newrelic.jar",
-            "AUTH_ENABLED":"true",
-#            "AUTH_ENDPOINT":"/api/auth/",
-#            "BENTO_API_VERSION":config[service]['image'],
-#            "MYSQL_SESSION_ENABLED":"true",
-#            "NEO4J_URL":"bolt://{}:7687".format(config['db']['neo4j_ip']),
-#            "REDIS_ENABLE":"false",
-#            "REDIS_FILTER_ENABLE":"false",
-#            "REDIS_HOST":"localhost",
-#            "REDIS_PORT":"6379",
-#            "REDIS_USE_CLUSTER":"true",
         }
 
     secrets={
@@ -58,15 +47,11 @@ class backendService:
             "MONGO_DB_PASSWORD":ecs.Secret.from_secrets_manager(self.secret, 'mongo_db_password'),
             "MONGO_DB_USER":ecs.Secret.from_secrets_manager(self.secret, 'mongo_db_user'),
             "DATABASE_NAME":ecs.Secret.from_secrets_manager(self.secret, 'database_name'),
-            "EMAIL_USER":ecs.Secret.from_secrets_manager(self.secret, 'email_user'),
-            "EMAIL_PASSWORD":ecs.Secret.from_secrets_manager(self.secret, 'email_password'),
-            "EMAIL_URL":ecs.Secret.from_secrets_manager(self.secret, 'email_url'),
-            "SUBMISSION_BUCKET":ecs.Secret.from_secrets_manager(self.secret, 'submission_bucket'),
         }   
     
     taskDefinition = ecs.FargateTaskDefinition(self,
         "{}-{}-taskDef".format(self.namingPrefix, service),
-        family=f"{config['main']['resource_prefix']}-{config['main']['tier']}-backend",
+        family=f"{config['main']['resource_prefix']}-{config['main']['tier']}-essentialvalidation",
         cpu=config.getint(service, 'cpu'),
         memory_limit_mib=config.getint(service, 'memory')
     )
@@ -79,7 +64,7 @@ class backendService:
         image=ecs.ContainerImage.from_ecr_repository(repository=ecr_repo, tag=config[service]['image']),
         cpu=config.getint(service, 'cpu'),
         memory_limit_mib=config.getint(service, 'memory'),
-        port_mappings=[ecs.PortMapping(app_protocol=ecs.AppProtocol.http, container_port=config.getint(service, 'port'), name=service)],
+        #port_mappings=[ecs.PortMapping(app_protocol=ecs.AppProtocol.http, container_port=config.getint(service, 'port'), name=service)],
         entry_point=entry_point,
         environment=environment,
         secrets=secrets,
@@ -90,10 +75,11 @@ class backendService:
 
     ecsService = ecs.FargateService(self,
         "{}-{}-service".format(self.namingPrefix, service),
-        service_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-backend",
+        service_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-essentialvalidation",
         cluster=self.ECSCluster,
         task_definition=taskDefinition,
         enable_execute_command=True,
+        desired_count=1,
         min_healthy_percent=50,
         max_healthy_percent=200,
         circuit_breaker=ecs.DeploymentCircuitBreaker(
@@ -102,32 +88,52 @@ class backendService:
         ),
     )
 
-    scalable_target = ecsService.auto_scale_task_count(
-        min_capacity=1,  # adjust as needed
-        max_capacity=2  # adjust as needed
+    #Attach scalable target
+    scalable_target = appscaling.ScalableTarget(self,
+        "{}-{}-scalableTarget".format(self.namingPrefix, service),
+        #service_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-essentialvalidation",
+        service_namespace=appscaling.ServiceNamespace.ECS,
+        #min_capacity=1,  # adjust as needed
+        #max_capacity=20  # adjust as needed
+        min_capacity=config.getint(service, 'autoscaling_min_capacity'),
+        max_capacity=config.getint(service, 'autoscaling_max_capacity'),
+        resource_id=f"service/{self.ECSCluster.cluster_name}/{ecsService.service_name}",
+        scalable_dimension="ecs:service:DesiredCount"
     )
 
-    scalable_target.scale_on_cpu_utilization(
-        "CpuScalingPolicy",
-        target_utilization_percent=80,  # target average CPU utilization
-        scale_in_cooldown=Duration.seconds(60),   # wait 60s before scaling in
-        scale_out_cooldown=Duration.seconds(60)   # wait 60s before scaling out
+    # Define CloudWatch metric for SQS ApproximateNumberOfMessagesVisible
+    queue_name = f"{config['main']['resource_prefix']}-{config['main']['tier']}-loader-queue.fifo"
+    sqs_metric = cloudwatch.Metric(
+        namespace="AWS/SQS",
+        metric_name="ApproximateNumberOfMessagesVisible",
+        dimensions_map={"QueueName": queue_name},
+        statistic="Minimum",
+        period=Duration.seconds(10)
     )
-    ecsTarget = self.listener.add_targets("ECS-{}-Target".format(service),
-        port=int(config[service]['port']),
-        protocol=elbv2.ApplicationProtocol.HTTP,
-        target_group_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-backend",
-        health_check = elbv2.HealthCheck(
-            path=config[service]['health_check_path'],
-            timeout=Duration.seconds(config.getint(service, 'health_check_timeout')),
-            interval=Duration.seconds(config.getint(service, 'health_check_interval')),),
-        targets=[ecsService],)
 
-    elbv2.ApplicationListenerRule(self, id="alb-{}-rule".format(service),
-        conditions=[
-            elbv2.ListenerCondition.host_headers(config[service]['host'].split(',')),
-            elbv2.ListenerCondition.path_patterns(config[service]['path'].split(','))
+    # Cloudwatch Alarm
+    scale_out_alarm = cloudwatch.Alarm(self,
+        "{}-{}-scalableTarget".format(self.namingPrefix, service),
+        alarm_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-essential-scale-out-alarm",
+        metric=sqs_metric,
+        threshold=1,
+        evaluation_periods=2,
+        comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
+    )
+
+    # Define step-out policy
+    scale_out_policy = scalable_target.scale_on_metric(
+        f"{config['main']['resource_prefix']}-{config['main']['tier']}-essential-scale-out",
+        metric=sqs_metric,
+        scaling_steps=[
+            appscaling.ScalingInterval(lower=1, upper=21, change=5),   # when 1 ≤ messages < 21 → add 5 tasks
+            appscaling.ScalingInterval(lower=21, change=20),           # when ≥ 21 → add 20 tasks
         ],
-        priority=int(config[service]['priority_rule_number']),
-        listener=self.listener,
-        target_groups=[ecsTarget])
+        adjustment_type=appscaling.AdjustmentType.CHANGE_IN_CAPACITY,
+        cooldown=Duration.seconds(300)
+    )
+
+    # Connect alarm to scale out policy
+    scale_out_alarm.add_alarm_action(
+        cw_actions.ApplicationScalingAction(scale_out_policy)
+    )
