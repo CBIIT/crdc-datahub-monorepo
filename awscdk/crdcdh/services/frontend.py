91b9f241d1e9e9052ca6077a11aa4f75e4039064
Tracy Truong <tracy18@users.noreply.github.com>
2025-06-11 18:00:09 -0400

    modified the cdk stack to add role and schedule scale

--- awscdk/crdcdh/services/frontend.py
+++ awscdk/crdcdh/services/frontend.py
@@ -1,8 +1,16 @@
+from aws_cdk import Duration
 from aws_cdk import aws_elasticloadbalancingv2 as elbv2
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_secretsmanager as secretsmanager
 from datetime import date
+from aws_cdk import Duration
+from aws_cdk import aws_applicationautoscaling as appscaling
+from aws_cdk import aws_cloudwatch as cloudwatch
+from aws_cdk import aws_cloudwatch_actions as cw_actions
+from aws_cdk import aws_sqs as sqs
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_s3 as s3
 
 class frontendService:
   def createService(self, config):
@@ -99,6 +107,156 @@ class frontendService:
         )
     )
 
+    #roles attached to ecs
+    bucket = s3.Bucket.from_bucket_name(self, f"{self.namingPrefix}-submission-ref", f"{self.namingPrefix}-submission")
+    # add s3 bucket policy to allow task def role to access submission bucket
+    bucket_submission_policy = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=[
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:ListBucket",
+            "s3:DeleteObject"
+        ],
+        resources=[
+            bucket.bucket_arn,
+            f"{bucket.bucket_arn}/*"
+        ]
+    )
+
+    data_sync_policy = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=[
+            "s3:PutObjectTagging",
+            "s3:ListObjectsV2",
+            "s3:ListBucket",
+            "s3:ListAllMyBuckets",
+            "s3:GetObjectVersionTagging",
+            "s3:GetObjectVersion",
+            "s3:GetObjectTagging",
+            "s3:GetObject",
+            "s3:GetBucketLocation",
+            "iam:ListRoles",
+            "iam:CreateRole",
+            "iam:CreatePolicy",
+            "iam:AttachRolePolicy",
+            "datasync:TagResource",
+            "datasync:StartTaskExecution",
+            "datasync:ListTasks",
+            "datasync:ListTaskExecutions",
+            "datasync:ListLocations",
+            "datasync:DescribeTaskExecution",
+            "datasync:DescribeTask",
+            "datasync:DescribeLocation*",
+            "datasync:DeleteTask",
+            "datasync:DeleteLocation",
+            "datasync:CreateTask",
+            "datasync:CreateLocationS3",
+            "datasync:CancelTaskExecution"
+        ],
+        resources=["*"]
+    )
+
+    # pass role in datasync
+    data_sync_pass_role = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=["iam:PassRole"],
+        resources=["*"],
+        conditions={
+            "StringEquals": {
+                "iam:PassedToService": "datasync.amazonaws.com"
+            }
+        }
+    )
+
+    # allowed access the other buckets
+    data_sync_other_buckets = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=[
+            "s3:ListObjectsV2",
+            "s3:ListBucketMultipartUploads",
+            "s3:ListBucket",
+            "s3:GetBucketLocation",
+            "s3:PutObjectTagging",
+            "s3:PutObject",
+            "s3:GetObjectTagging",
+            "s3:GetObject",
+            "s3:DeleteObject",
+            "s3:AbortMultipartUpload"
+        ],
+        resources=[
+            "arn:aws:s3:::nci-crdc-data-bucket-dev",
+            "arn:aws:s3:::icdc-cbiit-test-metadata",
+            "arn:aws:s3:::ctdc-cbiit-test-metadata",
+            "arn:aws:s3:::cds-cbiit-test-metadata",
+            "arn:aws:s3:::nci-crdc-data-bucket-dev/*",
+            "arn:aws:s3:::icdc-cbiit-test-metadata/*",
+            "arn:aws:s3:::ctdc-cbiit-test-metadata/*",
+            "arn:aws:s3:::cds-cbiit-test-metadata/*"
+        ]
+    )
+
+    # attach sqs iam access
+    sqs_iam_access = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=["sqs:*"],
+        resources=[
+            f"arn:aws:sqs:{config['main']['region']}:{config['main']['account_id']}:*"
+        ]
+    )
+
+    # attach quicksight embedded policy
+    quicksight_embed_policy = iam.PolicyStatement(
+        effect=iam.Effect.ALLOW,
+        actions=[
+            "quicksight:GetDashboardEmbedUrl",
+            "quicksight:GetAnonymousUserEmbedUrl",
+            "quicksight:GenerateEmbedUrlForRegisteredUser",
+            "quicksight:GenerateEmbedUrlForAnonymousUser"
+        ],
+        resources=[
+            f"arn:aws:quicksight:{config['main']['region']}:{config['main']['account_id']}:dashboard/*"
+        ]
+    )
+
+    # attach policy to the task role
+    taskDefinition.task_role.add_to_policy(bucket_submission_policy)
+    taskDefinition.task_role.add_to_policy(data_sync_policy)
+    taskDefinition.task_role.add_to_policy(data_sync_pass_role)
+    taskDefinition.task_role.add_to_policy(data_sync_other_buckets)
+    taskDefinition.task_role.add_to_policy(sqs_iam_access)
+    taskDefinition.task_role.add_to_policy(quicksight_embed_policy)
+
+    taskDefinition.execution_role.add_to_policy(bucket_submission_policy)
+    taskDefinition.execution_role.add_to_policy(data_sync_policy)
+    taskDefinition.execution_role.add_to_policy(data_sync_pass_role)
+    taskDefinition.execution_role.add_to_policy(data_sync_other_buckets)
+    taskDefinition.execution_role.add_to_policy(sqs_iam_access)
+    taskDefinition.execution_role.add_to_policy(quicksight_embed_policy)
+
+    # attach amazon managed policy to the task role
+    taskDefinition.task_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSQSFullAccess")
+    )
+
+    taskDefinition.task_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2ContainerServiceEventsRole")
+    )
+
+    taskDefinition.task_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchLogsFullAccess")
+    )
+
+    taskDefinition.execution_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSQSFullAccess")
+    )
+    taskDefinition.execution_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2ContainerServiceEventsRole")
+    )
+    taskDefinition.execution_role.add_managed_policy(
+        iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchLogsFullAccess")
+    )
+
     ecsService = ecs.FargateService(self,
         "{}-{}-service".format(self.namingPrefix, service),
         service_name=f"{config['main']['resource_prefix']}-{config['main']['tier']}-frontend",
@@ -112,6 +270,39 @@ class frontendService:
             rollback=True
         ),
     )
+
+    # added ecs run by schedule
+    scalable_target = ecsService.auto_scale_task_count(
+        min_capacity=1,  # adjust as needed
+        max_capacity=1  # adjust as needed
+    )
+
+    # scale on schedule
+    tier = config['main']['tier']
+    if tier.lower() != 'prod':
+        scalable_target.scale_on_schedule(
+            f"{config['main']['resource_prefix']}-{config['main']['tier']}-frontend-start",
+            schedule=appscaling.Schedule.cron(
+                minute="7",
+                hour="11",
+                week_day="MON-FRI"
+            ),
+            min_capacity=1,
+            max_capacity=1,
+            #schedule_time_zone="America/New_York"
+        )
+
+        scalable_target.scale_on_schedule(
+            f"{config['main']['resource_prefix']}-{config['main']['tier']}-frontend-stop",
+            schedule=appscaling.Schedule.cron(
+                minute="0",
+                hour="23",
+                week_day="MON-FRI"
+            ),
+            min_capacity=0,
+            max_capacity=0
+        #    schedule_time_zone="America/New_York"
+        )
 
     ecsTarget = self.listener.add_targets("ECS-{}-Target".format(service),
         port=int(config[service]['port']),
